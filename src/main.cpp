72b9bbc83fa26b5eac7c059880d2333b3a673a2c
VL <vincentlavalleewrk@outlook.com>
2023-10-27 13:02:04 -0400

    Commit 2

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ uint16_t rightEncoder = 0;
 int16_t diffEncodersLR = 0;
 int16_t diffEncodersRL = 0;
 
-float speed = 0.5;
+float speed = 0.2;
 
 const float Kp = 0.0025;
 // const float Ki = 0.01;
@@ -40,7 +40,7 @@ int wait = 1000;
 ////////////////////////////
 
 //>> Fonctions <<//
-void move();
+void move(float turnLeft, float turnRight);
 ///////////////////
 
 void setup(){
@@ -48,27 +48,56 @@ void setup(){
 }
 
 void loop(){
-  move();
+  move(1,1);
 }
 
-void move(){
+void move(float turnLeft, float turnRight){
 
-  MOTOR_SetSpeed(LEFT, speed);
-  MOTOR_SetSpeed(RIGHT,speed);
+  //if(turnRight != 1 || turnLeft != 1){
 
-  leftEncoder = ENCODER_Read(LEFT);
-  rightEncoder = ENCODER_Read(RIGHT);
+    leftEncoder = ENCODER_Read(LEFT);
+    rightEncoder = ENCODER_Read(RIGHT);
 
-  diffEncodersLR = leftEncoder - rightEncoder;
-  diffEncodersRL = rightEncoder - leftEncoder;
+    leftSpeed = speed * turnLeft; 
+    rightSpeed = speed * turnRight;
 
-  err_leftEncoder = Set_Point - (diffEncodersLR/2);
-  err_rightEncoder = Set_Point - (diffEncodersRL/2);
+    float ratioSpeedLR = leftSpeed / rightSpeed;
+    float ratioSpeedRL = rightSpeed / leftSpeed;
 
-  leftAdjust = Kp * err_leftEncoder;
-  rightAdjust = Kp * err_rightEncoder;
+    float ratioEncoderLR = leftEncoder / rightEncoder;
+    float ratioEncoderRL = rightEncoder / leftEncoder;
 
+    diffEncodersLR = ratioSpeedLR - ratioEncoderLR;
+    diffEncodersRL = ratioSpeedRL - ratioEncoderRL;
+
+    err_leftEncoder = Set_Point - (diffEncodersLR/2);
+    err_rightEncoder = Set_Point - (diffEncodersRL/2);
+
+    leftAdjust = Kp * err_leftEncoder;
+    rightAdjust = Kp * err_rightEncoder;
+
+    leftSpeed = (leftSpeed + leftAdjust);
+    rightSpeed = (rightSpeed + rightAdjust);
+  //}
+  // else{
+
+  //   leftEncoder = ENCODER_Read(LEFT);
+  //   rightEncoder = ENCODER_Read(RIGHT);
+
+  //   diffEncodersLR = leftEncoder - rightEncoder;
+  //   diffEncodersRL = rightEncoder - leftEncoder;
+    
+  //   err_leftEncoder = Set_Point - (diffEncodersLR/2);
+  //   err_rightEncoder = Set_Point - (diffEncodersRL/2);
+
+  //   leftAdjust = Kp * err_leftEncoder;
+  //   rightAdjust = Kp * err_rightEncoder;
+
+  //   leftSpeed = (speed + leftAdjust);
+  //   rightSpeed = (speed + rightAdjust);
+  // }
   
+
   if(millis() - previousTime >= wait){
     leftEncoder = ENCODER_ReadReset(LEFT);
     rightEncoder = ENCODER_ReadReset(RIGHT);
@@ -81,19 +110,13 @@ void move(){
     PRINT(" diffRL : ");
     PRINT((String)diffEncodersRL);
     PRINT(" L Speed : ");
-    PRINT((String)(leftAdjust));
+    PRINT((String)(leftSpeed));
     PRINT(" R Speed : ");
-    PRINTLN((String)(rightAdjust));
+    PRINTLN((String)(rightSpeed));
     previousTime = millis();
   }
 
-  leftSpeed = speed + leftAdjust;
-  rightSpeed = speed + rightAdjust;
-  
   MOTOR_SetSpeed(LEFT, leftSpeed);
   MOTOR_SetSpeed(RIGHT, rightSpeed);
-
-
-  
 
 }
