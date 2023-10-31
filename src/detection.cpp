b7353f15793b4bb75068301a487e19b92a1e3cde
AntoineMartin8 <mara2040@usherbrooke.ca>
2023-10-31 16:01:06 -0400

    wip

--- src/detection.cpp
+++ src/detection.cpp
@@ -1,5 +1,19 @@
 #include "detection.hpp"
 
+float previousDistance;
+float currentDistance;
+float rightCorrection = 1;
+float leftCorrection = 1;
+
+unsigned int distance = 0;
+unsigned int correctionFactor = 17158;
+float exponent_Factor = -0.797;
+
+unsigned int avalogValue = 0;
+unsigned long cmptData = 0;
+
+static int last_time = 0;
+
 bool cupGreen() {
     int green = digitalRead(GREEN);
     int red = digitalRead(RED);
@@ -7,9 +21,52 @@ bool cupGreen() {
 }
 
 bool cupYellow() {
-    float voltage = analogRead(RIGHT_SENSOR);
-    return((voltage > 0.6) ? true : false);
-   
+    float distance = distanceTOF_mm();
+    return((distance > CUP_DISTANCE) ? true : false);
+}
+
+void followWall() {
+    previousDistance = distanceTOF_mm();
+
+    #ifdef DEBUG
+        Serial.println(previousDistance);
+    #endif
+
+    MOTOR_SetSpeed(LEFT, 0.3 * leftCorrection);
+    MOTOR_SetSpeed(RIGHT, 0.3 * rightCorrection);
+    currentDistance = analogRead(RIGHT_SENSOR);
+    if(currentDistance != TARGET_DISTANCE && previousDistance/currentDistance != 1.0f)
+        orientationFactor();
+}
+
+void orientationFactor() {
+    if(previousDistance/currentDistance > 1) {
+        leftCorrection = 0.9;
+        rightCorrection = 1.1;
+    }
+    else if (previousDistance/currentDistance < 1) {
+        leftCorrection = 1.1;
+        rightCorrection = 0.9;
+    }
+}
+
+int distanceTOF_mm(){
+
+  unsigned int distanceTOF_mm = 0;
+
+ 
+  int current_time = millis();
+
+  if(current_time - last_time >= 1000){
+    avalogValue = analogRead(RIGHT_SENSOR);
+    distanceTOF_mm = (float) correctionFactor * pow((float) avalogValue, exponent_Factor);
+
+    cmptData ++;
+
+    last_time = current_time;
+    
+    return distanceTOF_mm;
+  }
 }
 
 void DetectionInit(void) {
@@ -17,4 +74,7 @@ void DetectionInit(void) {
     pinMode(GREEN, INPUT);
     pinMode(RED, INPUT);
     pinMode(RIGHT_SENSOR, INPUT);
+
+    leftCorrection = 1;
+    rightCorrection = 1;
 }
