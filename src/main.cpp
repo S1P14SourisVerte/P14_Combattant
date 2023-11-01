be77080e6a19bdea0ff7fcca84e72bdc0c6e764e
felixmalhaire <felixmalhaire@gmail.com>
2023-11-01 19:50:08 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ void loop() {
   }
   if (active) {
     start_color = Floor_Color();
-    move(0.3, 5, false, false) //pour détecter la couleur puis avancer au tape
+    move(0.3, 5, false, false); //pour détecter la couleur puis avancer au tape
     //CASE 1
     smoothTurn(0.3, start_color);
     //CASE 2
