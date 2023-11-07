f88393844121a95a8ec15252eeb80171f47b7d80
MaximeEgolf <egom1401@usherbrooke.ca>
2023-11-06 19:37:39 -0500

    l

---
+++ src/zigzag_detection.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Librobus.h>
+
+// PAS D'UTILISATION DE BOUCLE WHILE, car on veut que le robot puisse faire d'autre chose en avançant
+
+void zigzag(){
+  
+}
+
+void setup(){
+  BoardInit();
+}
+
+void loop(){
+  int b;
+}
